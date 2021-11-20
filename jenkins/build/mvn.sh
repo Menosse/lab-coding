d2d450ff68b00b6cb0d275d3444c9d003782c6e4
Fernando Takada <fernando.b.takada@rakuten.com>
2021-11-20 13:12:46 +0900

    jenkins pipeline

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 # WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/vorx-backend-app-pipeline
 WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/lab-coding
 
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
+docker run --rm -v $WORKSPACE/Projects/fit/desafio-imc:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
